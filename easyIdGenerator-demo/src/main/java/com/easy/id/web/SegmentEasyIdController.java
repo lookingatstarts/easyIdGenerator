19a53eab2892acf92591205709c25ebe305d5ccf
zhangbingbing <lookingatstarts@163.com>
2021-12-04 14:41:36 +0800

    兼容

--- easyIdGenerator-demo/src/main/java/com/easy/id/web/SegmentEasyIdController.java
+++ easyIdGenerator-demo/src/main/java/com/easy/id/web/SegmentEasyIdController.java
@@ -4,6 +4,7 @@ import com.easy.id.service.segment.SegmentEasyIdService;
 import com.easy.id.web.resp.ApiResponse;
 import org.hibernate.validator.constraints.Range;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +22,7 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/segment/ids")
 @Validated
+@ConditionalOnBean(SegmentEasyIdService.class)
 public class SegmentEasyIdController {
 
     @Autowired
--- easyIdGenerator-demo/src/main/java/com/easy/id/web/SnowflakeEasyIdController.java
+++ easyIdGenerator-demo/src/main/java/com/easy/id/web/SnowflakeEasyIdController.java
@@ -4,6 +4,7 @@ import com.easy.id.service.snowflake.SnowflakeEasyIdService;
 import com.easy.id.web.resp.ApiResponse;
 import org.hibernate.validator.constraints.Range;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,7 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/snowflake/ids")
 @Validated
+@ConditionalOnBean(SnowflakeEasyIdService.class)
 public class SnowflakeEasyIdController {
 
     @Autowired
